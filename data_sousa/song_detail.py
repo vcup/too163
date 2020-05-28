7e6c890fa561cbfb9ca50eaa454fc919e888b038
Vcup <vcupmoe@gmail.com>
2020-05-28 21:26:22 +0800

    添加了类型注释

--- data_sousa/song_detail.py
+++ data_sousa/song_detail.py
@@ -1,5 +1,5 @@
 from data_sousa import key_path
-from typing import Union
+from typing import Union, Generator, Any
 
 
 class song_detail_old:
@@ -11,12 +11,11 @@ class song_detail_old:
         self.path = key_path(data, point='songs')
         self.len = len(self.path)
 
-
     def album(self, i: int = 0) -> dict:
         """指定索引位置的歌曲的专辑信息"""
         return self.path / f'[{i}]/album'
 
-    def album_iter(self) -> iter:
+    def album_iter(self) -> Generator[dict, Any, None]:
         """迭代所有请求歌曲的专辑信息"""
         return (self.album(i) for i in range(self.len))
 
@@ -24,11 +23,11 @@ class song_detail_old:
         """指定索引位置的歌曲的单个歌手信息"""
         return self.path / f'[{i1}]/artists/[{i2}]'
 
-    def artist_iter(self, i1: int = 0) -> artist:
+    def artist_iter(self, i1: int = 0) -> Generator[dict, Any, None]:
         """迭代指定索引位置的歌曲的所有歌手信息"""
         return (self.artist(i1, i2) for i2 in range(len(self.path / f'[{i1}]/artists')))
 
-    def all_aritst_iter(self) -> artist_iter:
+    def all_aritst_iter(self) -> Generator[dict, Any, None]:
         """迭代所有请求歌曲的所有歌手信息"""
         return (a for i1 in range(self.len) for a in self.artist_iter(i1))
 
@@ -36,31 +35,31 @@ class song_detail_old:
         """指定索引位置的歌曲的单个别名"""
         return self.path / f'[{i1}]/alias/[{i2}]'
 
-    def alias_iter(self, i1: int = 0) -> alias:
+    def alias_iter(self, i1: int = 0) -> Generator[str, Any, None]:
         """迭代指定索引位置的歌曲的所有别名"""
-        return (self.artist(i1, i2) for i2 in range(len(self.path / f'[{i1}]/artists')))
+        return (self.alias(i1, i2) for i2 in range(len(self.path / f'[{i1}]/artists')))
 
-    def all_alias_iter(self) -> alias_iter:
+    def all_alias_iter(self) -> Generator[dict, Any, None]:
         """迭代所有请求歌曲的所有别名"""
         return (a for i1 in range(self.len) for a in self.artist_iter(i1))
 
-    def name(self, i: int = 0):
+    def name(self, i: int = 0) -> str:
         """指定索引位置的歌曲的名字"""
         return self.path / f'[{i}]/name'
 
-    def name_iter(self):
+    def name_iter(self) -> Generator[str, Any, None]:
         """迭代所有请求歌曲的名字"""
         return (self.name(i) for i in range(self.len))
 
-    def id(self, i: int = 0):
+    def id(self, i: int = 0) -> int:
         """指定索引位置的歌曲的ID"""
         return self.path / f'[{i}]/id'
 
-    def id_iter(self):
+    def id_iter(self) -> Generator[int, Any, None]:
         """迭代所有请求歌曲的ID"""
         return (self.id(i) for i in range(self.len))
 
-    def mvid(self, i: int = 0):
+    def mvid(self, i: int = 0) -> int:
         """指定索引位置的歌曲的MV的ID"""
         return self.path / f'[{i}]/mvid'
 
