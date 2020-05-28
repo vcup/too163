f82911bd0a897da6663d28a25bef9d8cf73ff49c
Vcup <vcupmoe@gmail.com>
2020-05-28 21:30:17 +0800

    添加了类型标注

--- data_sousa/playlist.py
+++ data_sousa/playlist.py
@@ -1,4 +1,4 @@
-from typing import Union, Generator
+from typing import Union, Generator, Any
 from data_sousa import key_path
 
 
@@ -11,20 +11,20 @@ class playlist_old:
     def track(self, i: int = 0) -> dict:
         return self.path / f'tracks/[{i}]'
 
-    def track_iter(self)-> Generator[dict]:
+    def track_iter(self)-> Generator[dict, Any, None]:
         tracks_len = len(self.path / 'tracks')
         return (self.track(i) for i in range(tracks_len))
 
     def sid(self, i: int = 0) -> int:
         return self.track(i).get('id')
 
-    def sid_iter(self) -> Generator[int]:
+    def sid_iter(self) -> Generator[int, Any, None]:
         return (s.get('id') for s in self.track_iter())
 
     def name(self, i: int = 0) -> str:
         return self.track(i).get('id')
 
-    def name_iter(self) -> Generator[str]:
+    def name_iter(self) -> Generator[str, Any, None]:
         return (s.get('name') for s in self.track_iter())
 
 
@@ -34,7 +34,7 @@ class playlist_new(playlist_old):
         """歌单中指定位置的歌曲的一些信息"""
         return self.path / f'trackIds/[{i}]'
 
-    def trackIds_iter(self) -> Generator[dict]:
+    def trackIds_iter(self) -> Generator[dict, Any, None]:
         """迭代歌单中包含的的全部歌曲的信息"""
         tracks_len = len(self.path / 'trackIds')
         return (self.trackIds(i) for i in range(tracks_len))
@@ -44,7 +44,7 @@ class playlist_new(playlist_old):
         self.path.set_back(1)
         return self.path / f'privileges/[{i}]'
 
-    def privileges_iter(self) -> Generator[dict]:
+    def privileges_iter(self) -> Generator[dict, Any, None]:
         tracks_len = (self.path / 'privileges')
         return (self.privileges(i) for i in range(tracks_len))
 
@@ -52,7 +52,7 @@ class playlist_new(playlist_old):
         """歌单中指定位置的歌曲id"""
         return self.trackIds(i).get('id')
 
-    def sid_iter(self) -> Generator[int]:
+    def sid_iter(self) -> Generator[int, Any, None]:
         """迭代歌单中全部歌曲的id"""
         return (s.get('id') for s in self.trackIds_iter())
 
