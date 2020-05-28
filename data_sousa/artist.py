b3f62185841171eed2fa29dd689574d027ccf7aa
Vcup <vcupmoe@gmail.com>
2020-05-28 20:23:58 +0800

    添加alias及其生成器

--- data_sousa/artist.py
+++ data_sousa/artist.py
@@ -11,5 +11,12 @@ class artist_old:
         self.len = len(self.path)
 
 
+    def alias(self, i: int = 0):
+        return self.path / f'alias/[{i}]'
+
+    def alias_iter(self):
+        return (self.alias(i) for i in range(self.len))
+
+
 def artist(data: dict) -> Union[artist_old]:
     return artist_old(data)
