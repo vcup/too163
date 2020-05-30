79d0ad23d6c6dca4d52bc35787c4aef90a4d33f9
Vcup <vcupmoe@gmail.com>
2020-05-30 21:14:38 +0800

    添加more属性，添加类型标注

--- data_sousa/user_playlist.py
+++ data_sousa/user_playlist.py
@@ -1,5 +1,5 @@
 from data_sousa import key_path
-from typing import Union
+from typing import Union, Generator, Any
 
 
 class user_playlist_old:
@@ -8,16 +8,17 @@ class user_playlist_old:
     def __init__(self, data: dict):
         self.path = key_path(data, point='playlist')
         self.len = len(self.path)
+        self.more = data.get('more')
 
     def list(self, i: int = 0) -> dict:
         return self.path / f'[{i}]'
 
-    def list_iter(self) -> iter:
+    def list_iter(self) -> Generator[dict, Any, None]:
         return (self.list(i) for i in range(self.len))
 
 
 class user_playlist_new(user_playlist_old):
-    """解析来自"""
+    pass
 
 
 def user_playlist(data: dict) -> Union[user_playlist_old, user_playlist_new]:
