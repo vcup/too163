f66b7c0787bc34e78b1d4176a8dd4b4bbc197ad2
Vcup <vcupmoe@gmail.com>
2020-06-05 19:49:42 +0800

    删除了dbis函数以及三个迭代器函数(迭代歌单内歌曲、迭代用户歌单，迭代用户所有歌曲)

--- tools.py
+++ tools.py
@@ -55,46 +55,6 @@ def database_add_playlist_song_id(pid):
     conn.close()
 
 
-def usr_all_play_list_gen(uid: int) -> iter:
-    user_playlist_dict = api.user_playlist(uid)
-    user_playlist = data_sousa.user_playlist(user_playlist_dict)
-    return (data_sousa.playlist(play_list) for play_list in user_playlist.list_iter())
-
-
-def play_list_all_song_info(pid: int) -> iter:
-    play_list_dict = api.playlist(pid)
-    play_list = data_sousa.playlist(play_list_dict)
-    for sid in play_list.sid_iter():
-        song_info = api.song_detail(sid)
-        i = data_sousa.song_detail(song_info)
-        yield {'name': i.name(), 'id': i.id(), 'artist': ','.join((x.get('name') for x in i.artist_iter()))}
-
-
-def usr_all_song_info_of_playlist_gen(uid: int) -> iter:
-    return (info_ for playlist in usr_all_play_list_gen(uid) for info_ in play_list_all_song_info(playlist.path / 'id'))
-
-
-def dbis(uid):
-    """把指定用户的所有歌曲加入数据库"""
-    conn = sqlite3.connect('ids.db')
-    cursor = conn.cursor()
-    cursor.execute(r"select count(*) from sqlite_master where type='table' and name = 'user_all_song_list_id'")
-    if cursor.fetchall() == [(0,)]: # 如果数据库中没有一个叫user_all_song_list_id的表，就创建一个
-        cursor.execute(r"create table user_all_song_list_id (type varchar(4), id int(14) PRIMARY KEY, name varchar(255))")
-    for s_info in usr_all_song_info_of_playlist_gen(uid):
-        sid = s_info.get('id')
-        name = s_info.get('name')
-        artist = s_info.get('artist')
-        print(f'{name} - {artist}')
-        try:
-            cursor.execute(r"INSERT INTO user_all_song_list_id VALUES ('song', ?, ?)", (sid, f'{name} - {artist}'))
-        except sqlite3.IntegrityError as error:
-            pass
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-
 def g_lyric():
     """取出数据库中的所有歌曲ID，批量下载歌词"""
     conn = sqlite3.connect('ids.db')
