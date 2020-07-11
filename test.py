bd45736b65229d17200c0a8eeffb5b4927d44de9
vcup <vcupmoe@gmail.com>
2020-07-11 20:02:40 +0800

    添加song的测试文件

--- test.py
+++ test.py
@@ -1,16 +1,31 @@
 import unittest
 
 import test_item
-from data_sousa import album
+from data_sousa import song_detail, album
 
 
 class TestDataSousa(unittest.TestCase):
+
+    class TestSong(unittest.TestCase):
+        @classmethod
+        def setUpClass(cls) -> None:
+            cls.song_dict_751472 = test_item.song_751472
+            cls.song_dict_22808851 = test_item.song_22808851
+            cls.song_dict_1293905025 = test_item.song_1293905025
+            # cls.song = song_detail(cls.song_dict_751472, cls.song_dict_22808851, cls.song_dict_1293905025)
+            cls.song_751472 = song_detail(cls.song_dict_751472)
+            cls.song_22808851 = song_detail(cls.song_dict_22808851)
+            cls.song_1293905025 = song_detail(cls.song_dict_1293905025)
 
     class TestAlbum(unittest.TestCase):
 
         @classmethod
         def setUpClass(cls) -> None:
-            cls.album = album(test_item.album_3308499)
+            cls.album_dict = test_item.album_3308499
+            cls.album = album(cls.album_dict)
+
+        def test_songs(self):
+            self.assertEqual('', self.album.songs())
 
 
 if __name__ == '__main__':
--- test_item/__init__.py
+++ test_item/__init__.py
@@ -1,9 +1,12 @@
 from json import load as _load
 
 
-def _open_json(file_path: str) -> _load:
+def __open_json(file_path: str) -> dict:
     with open(file_path, 'r') as file:
         return _load(file)
 
 
-album_3308499: dict = _open_json('album_3308499.json')
+album_3308499 = __open_json('album_3308499.json')
+song_751472 = __open_json('song_751472.json')
+song_22808851 = __open_json('song_22808851.json')
+song_1293905025 = __open_json('song_1293905025.json')
